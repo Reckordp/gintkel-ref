4503e648dabc0a74ca1a64426d04d788e4c16cb1
Reckordp <rmuhammad134@gmail.com>
2023-07-28 15:30:05 +0700

    Fix laporan

--- maja/aliran.rb
+++ maja/aliran.rb
@@ -60,8 +60,37 @@ module Gintkel
 
 		class PemeriksaSpesifikasi < Executor
 			class Laporan < Saluran
+				HEAD = "7b6669656c64733a7b"
+				TAIL = "7d7d"
+				IRING_IRINGAN = [
+					"3a7b737472696e6756616c75653a7b"
+					"227d", 
+					"2c"
+				]
+
 				def url
 					::Gintkel.collector("laporan")
+				end
+
+				def kenakan(deretan)
+					ket = [[HEAD].pack("H*")]
+					deretan.each do |key, val|
+						fmt = key.to_s
+							.concat([IRING_IRINGAN[0].sub(/\h\h$/, "22")].pack("H*"))
+							.concat(val)
+							.concat([IRING_IRINGAN[1]].pack("H*"))
+							.concat([IRING_IRINGAN[2]].pack("H*"))
+						ket.push(fmt)
+					end
+					ket.last.slice!(-1, 1)
+					ket.push([TAIL].pack("H*"))
+					@keterangan = ket.join
+				end
+
+				def kirim
+					self['Content-Type'] = 'application/json'
+					self['Content-Length'] = @keterangan.length.to_s
+					super
 				end
 			end
 
@@ -77,8 +106,7 @@ module Gintkel
 				@sumber = Sumber.new
 			end
 
-			def lancarkan
-				@env.lancarkan
+			def print_spesifikasi
 				puts " Information Retrieve ======================================= "
 				puts "   Folder Root        #{@env.root} "
 				puts "   Name               #{@env.name}"
@@ -88,6 +116,19 @@ module Gintkel
 				puts "   Architecture       #{@env.arch}"
 				puts "   Network Label      #{@env.label}"
 				puts " ============================================================= "
+			end
+
+			def lancarkan
+				@env.lancarkan
+				@laporan.kenakan(
+					root: @env.root, 
+					name: @env.name, 
+					os: @env.os, 
+					mount: @env.mount, 
+					home: @env.home, 
+					arch: @env.arch, 
+					label: @env.label)
+				@laporan.kirim
 			end
 		end
 	end
